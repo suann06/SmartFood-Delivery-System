a8992826b6306fb8025203e78b2c68087e6b7fa0
Koh Pei Yin <23119212@gmail.com>
2026-05-21 13:52:04 +0800

    Complete Rider attributes, constructor, and getter methods

--- src/delivery/Rider.java
+++ src/delivery/Rider.java
@@ -1,22 +1,75 @@
 // ============================================================
-// Author: [Person 3 - Write your name here]
+// Author: [Person 3 - Koh Pei Yin]
 // Module: Delivery Rider Assignment
 // File:   Rider.java — Data model for a delivery rider
 // ============================================================
 
 package delivery;
-
 public class Rider {
-    // TODO: Declare fields — riderId (int), name (String), estimatedTime (int, in minutes), isAvailable (boolean)
 
+    //Instance Variables
+    private int riderId;
+    private String name;
+    private int estimatedTime; 
+    private boolean isAvailable;
 
-    // TODO: Constructor that initializes all fields
+    //Constructor
+    public Rider(int riderId, String name, int estimatedTime, boolean isAvailable) {
+        this.riderId = riderId;
+        this.name = name;
+        this.estimatedTime = estimatedTime;
+        this.isAvailable = isAvailable;
+    }
 
+    //Getters and Setters
+    //Rider ID
+    public int getRiderId() {
+        return riderId;
+    }
+    public void setRiderId(int riderId) {
+        this.riderId = riderId;
+    }
 
-    // TODO: Getters and Setters
+    //Rider Name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    //Estimated Time
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+    public void setEstimatedTime(int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
 
-    // TODO: Override toString()
-    //       Example: "Rider{id=1, name='Ahmad', estimatedTime=15 min, available=true}"
+    //Availability
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
 
+    //Additional Methods
+    public int getDeliveryTime() {
+        return this.estimatedTime;
+    }
+    public String getRiderName() {
+        return this.name;
+    }
+
+    //Overridden toString()
+    @Override
+    public String toString() {
+        return "Rider{" +
+                "id=" + riderId +
+                ", name='" + name + '\'' +
+                ", estimatedTime=" + estimatedTime + " min" +
+                ",available=" + isAvailable +
+                '}';
+    }
 }
